5f407761eaf6fdd1abf77303d464b39caa1d7afe
Aarav-Gandani <82732060+Aarav-Gandani@users.noreply.github.com>
2022-04-21 18:22:15 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,1 +1,11 @@
-;
+var firebaseConfig = {
+    apiKey: "AIzaSyCb8Pf5B9IdAJITeC6tIq48Z61VwYMOHKA",
+    authDomain: "lets-chat-web-app-72773.firebaseapp.com",
+    projectId: "lets-chat-web-app-72773",
+    storageBucket: "lets-chat-web-app-72773.appspot.com",
+    messagingSenderId: "467687213799",
+    appId: "1:467687213799:web:f1dc937633ea12d12eb042"
+  };
+
+  // Initialize Firebase
+  var app = initializeApp(firebaseConfig);
